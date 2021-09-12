7b3c9cb370a8140bf503d22bf6c1d4cdb71c03ff
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-09-13 03:10:48 +0600

    problem in adding all price

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,19 +4,29 @@ import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
-    const total = cart.reduce((total, product) => total = (total + (product.price * product.quantity)), 0);
-    const shipping = cart.reduce((shipping, product) => shipping = (shipping + product.shipping), 0);
+    console.log(cart);
     const formatNumber = num => {
         const precision = num.toFixed(2);
         return Number(precision);
     }
+    // const total = cart.reduce((total, product) => total = (total + (product.price * product.quantity)), 0);
+
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price * product.quantity;
+        debugger;
+    }
+    console.log(total);
+
+    const shipping = cart.reduce((shipping, product) => shipping = (shipping + product.shipping), 0);
     return (
         <div className="text-lg-end rounded shadow p-3">
             <h1>Order Summary</h1>
             <h4>Items Ordered : {cart.length} </h4>
             <h4>Total: ${formatNumber(total)} </h4>
             <h4>Shipping: ${formatNumber(shipping)}</h4>
-           <Link to='/review'><Button variant='dark'>Review Order</Button></Link>
+            <Link to='/review'><Button variant='dark'>Review Order</Button></Link>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import fakeData from './../../fakeData/index';
@@ -9,17 +9,30 @@ const Shop = () => {
     const first15 = fakeData.slice(0, 15);
     // eslint-disable-next-line
     const [products, setProducts] = useState(first15);
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const savedCart = getDatabaseCart()
+        const savedKeys = Object.keys(savedCart);
+        const prevCart = savedKeys.map(existingKey => {
+            const product = fakeData.find(pd => pd.key === existingKey);
+            product.quantity = savedKeys[existingKey];
+            return product;
+        })
+        setCart(prevCart);
+    }, []);
+
     const handleAddProduct = (product) => {
         const toBeAddedKey = product.key;
         const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
         let count = 1;
         let newCart;
         if (sameProduct) {
-            const count = sameProduct.quantity + 1;
+            count = sameProduct.quantity + 1;
             sameProduct.quantity = count;
-            const others = cart.filter(pd => pd.key !== toBeAddedKey)
+            const others = cart.filter(pd => pd.key !== toBeAddedKey);
             newCart = [...others, sameProduct];
+            console.log(sameProduct);
         } else {
             product.quantity = 1;
             newCart = [...cart, product];
